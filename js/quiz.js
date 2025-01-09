f5415b2aa43352ec14c039b1b6b6f84f4446a770
yurei <167486448+yurei0903@users.noreply.github.com>
2025-01-09 16:12:53 +0900

    Add files via upload

--- js/quiz.js
+++ js/quiz.js
@@ -2,6 +2,7 @@ var hantei=true;
 var lock=true;
 var nitaku="０";
 var sentaku="０";
+const a=["0","1","2","3","4"]
 function nitakuFunction(event) {
     const sentakukaitou=document.getElementById('sentakukaitou');
     // クリックされたボタンのデータ属性からidを取得
@@ -14,18 +15,16 @@ function nitakuFunction(event) {
 function sentakuFunction(event) {
     // クリックされたボタンのデータ属性からidを取得
     const buttonId = event.target.getAttribute('data-id');
-    console.log(buttonId);
     sentaku=buttonId;
     sentakukaitou.innerText="現在の解答は"+sentaku+"です";
 }
-async function quizdasu() {
+async function quizdasu(pos) {
     try {
-        console.log("quizdasu");
         if(lock){
         const response = await fetch('JSON/questions.json');
         const questionData = await response.json();
         // loadQuestionがPromiseを返すようにします
-        return await loadQuestion(questionData);
+        return await loadQuestion(questionData,pos);
         }
     } catch (error) {
         console.error('Error loading the question data:', error);
@@ -34,11 +33,43 @@ async function quizdasu() {
 }
 
 // 問題をロードする関数
-function loadQuestion(questionData) {
+function loadQuestion(questionData,pos) {
+    
+    var basho=[0,0];
+    var Index="0";
+    console.log(pos)
     return new Promise((resolve) => {
-        console.log("loadQuesution");
-        const randomIndex = Math.floor(Math.random() * questionData.length);
-        const selectedQuestion = questionData[randomIndex];
+        switch(pos.x){
+        case 0:
+        case 5:basho[0]=0;break;
+        case 1:
+        case 4:basho[0]=1;break;
+        case 2:
+        case 3:basho[0]=2;break;
+        }
+        switch(pos.y){
+            case 0:
+            case 5:basho[1]=0;break;
+            case 1:
+            case 4:basho[1]=1;break;
+            case 2:
+            case 3:basho[1]=2;break;
+            default:basho[1]=2
+            }
+    
+    switch(basho[1]+basho[0]){//場所で難易度を調整してる
+        case 0: Index="2";break;
+        case 1: 
+        case 2: Index="1";break;
+        case 3:
+        case 4: Index="0";break;
+        default:Index="0";break;
+        }
+        const selectionData=questionData.filter(item=> item.level == Index);
+        console.log(selectionData);
+        const randamIndex = Math.floor(Math.random() * selectionData.length);
+        const selectedQuestion = selectionData[randamIndex];
+        
         console.log(selectedQuestion.答え)
         const questionArea = document.getElementById('question-area');
         const answerArea = document.getElementById('answer-area');
@@ -48,7 +79,6 @@ function loadQuestion(questionData) {
         quizElement.style.display = 'block'; // 要素を表示
 
         if (selectedQuestion.問題形式 === "一問一答") {
-            // questionArea.innerHTML += '<button id="answer">答えを確認</button>';
             answerArea.innerHTML +='<div id="answer" class="button"><a href="#">回答</a></div>'
             answerArea.innerHTML +='<input type="text" id="kaitou" placeholder="ここに文字を入力" />'
         }
@@ -57,7 +87,6 @@ function loadQuestion(questionData) {
             answerArea.innerHTML += '<button class="nitaku" data-id="１">'+nitakuadata[0]+'</button>';
             answerArea.innerHTML += '<button class="nitaku" data-id="２">'+nitakuadata[1]+'</button>';
             answerArea.innerHTML +='<div id="answer" class="button"><a href="#">回答</a></div>'
-            // questionArea.innerHTML += '<button id="answer">答えを確認</button>';
             const nitaku = document.querySelectorAll(".nitaku");
             nitaku.forEach(button => {
                 button.addEventListener('click', nitakuFunction);
@@ -69,7 +98,6 @@ function loadQuestion(questionData) {
             answerArea.innerHTML += '<button class="sentaku" data-id="２">'+sentakuadata[1]+'</button>';
             answerArea.innerHTML += '<button class="sentaku" data-id="３">'+sentakuadata[2]+'</button>';
             answerArea.innerHTML +='<div id="answer" class="button"><a href="#">回答</a></div>'
-            // questionArea.innerHTML += '<button id="answer">答えを確認</button>';
             const sentaku = document.querySelectorAll(".sentaku");
             sentaku.forEach(button => {
                 button.addEventListener('click', sentakuFunction);
@@ -95,7 +123,6 @@ function loadQuestion(questionData) {
 
 // 答えを確認する関数
 function checkAnswer(correctAnswer, format) {
-    console.log("checkAnswer");
     const  seikai= document.getElementById('seikai');
     const  matigai= document.getElementById('matigai');
     if (format === "一問一答") {
@@ -119,7 +146,6 @@ function checkAnswer(correctAnswer, format) {
     } else if (format === "択一(文章)" ) {
         userAnswer = sentaku;
         // 択一の確認
-        console.log(userAnswer)
         if (userAnswer=== correctAnswer.charAt(0)) {
             seikai.currentTime = 0; // 音声の再生位置をリセット
             seikai.play(); // 音を再生
@@ -137,7 +163,6 @@ function checkAnswer(correctAnswer, format) {
     }
     else if (format === "二択" ) {
         userAnswer = nitaku;
-        console.log(userAnswer)
         // 択一の確認
         if (userAnswer=== correctAnswer.charAt(0)) {
             seikai.currentTime = 0; // 音声の再生位置をリセット
@@ -154,12 +179,12 @@ function checkAnswer(correctAnswer, format) {
         }
     }
     
-    const parent = document.getElementById('question-area');
+    const parent = document.getElementById('answer-area');
     while(parent.firstChild){
     parent.removeChild(parent.firstChild);
     }
     sentakukaitou=document.getElementById('sentakukaitou');
-    sentakukaitou.innerHTML=""
+    sentakukaitou.innerHTML="";
     nitaku="０";
     sentaku="０";
     lock=true;
